7934e67e3969b9f5a40162e63954ee54e74128cb
Gizem Malçok <64318805+gizemgizg@users.noreply.github.com>
2022-03-11 09:29:36 +0300

    unit test setUp method added.

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -5,9 +5,14 @@ import 'package:festivalproject/home_screen/viewmodel/home_viewmodel.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
+  
+  late HomeViewModel homeViewModel;
+  
+  setUp((){
+    homeViewModel = HomeViewModel();
+  });
 
-  test("getData ", () async{
-    final homeViewModel = HomeViewModel();
+  test("get data test", () async{
     final response = await homeViewModel.getData();
     expect(response is HomeScreenModel, true);
   });
